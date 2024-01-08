1bb630c2abebca453908c1980f0ce64213258ca2
Daniel M. Tarrio Guri <daniel.tarrio@gmail.com>
2024-01-07 21:58:23 -0300

    Carga archivos con prototipo de reproducto de video

--- js.js
+++ js.js
@@ -1,37 +1,49 @@
 
 /*{}*/
 let video="";
-let myInterval="";
+let mi_intervalo="";
 
-const reproducir=(x)=>{
-    video=document.getElementById(x);
+const reproducir=(x,y)=>{
+    video=document.getElementById(y);
     let dur_min=parseInt(video.duration/60);
     let dur_seg=parseInt(video.duration-(dur_min*60));
-    document.getElementById("duracion").innerText=dur_min+"m:"+dur_seg+"s";
+    const number = 1
+    dur_min=dur_min.toString().padStart(2, '0');
+    dur_seg=dur_seg.toString().padStart(2, '0');
+    document.getElementById("duracion").innerText=dur_min+":"+dur_seg;
     if(video.paused==false){
         video.pause();
-        myStopFunction();
+        x.classList.add('ctrl_play');
+        x.classList.remove('ctrl_pause');
+        stop_temporizador();
     }else{
         video.play();
-        myTimer()
+        x.classList.remove('ctrl_play');
+        x.classList.add('ctrl_pause');
+        temporizador()
     }
 }
 
-
-function myTimer() {
-    myInterval = setInterval(myTimer, 1000);
+const temporizador=()=> {
+    mi_intervalo = setInterval(temporizador, 1000);
     let rep_min=parseInt(video.currentTime/60);
     let rep_seg=parseInt(video.currentTime-(rep_min*60));
-    document.getElementById("reproducido").innerText=rep_min+"m:"+rep_seg+"s";
-    document.getElementById("mi_barra").value=parseFloat((video.currentTime/video.duration)*100)
+    rep_min=rep_min.toString().padStart(2, '0');
+    rep_seg=rep_seg.toString().padStart(2, '0');
+    document.getElementById("reproducido").innerText=rep_min+":"+rep_seg;
+    document.getElementById("mi_barra").value=parseFloat((video.currentTime/video.duration)*100);
     if(video.duration==video.currentTime){
-        myStopFunction();
+        stop_temporizador();
     }
-
 }
 
-function myStopFunction() {
-  clearInterval(myInterval);
+const avance=(x)=>{
+    video=document.getElementById(x);
+    video.currentTime=video.duration*(document.getElementById("mi_barra").value/100);
+}
+
+const stop_temporizador=()=>{
+  clearInterval(mi_intervalo);
 }
 
 
