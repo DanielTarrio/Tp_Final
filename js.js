a374a4ce4fcbded1807644a499fc87aa0ebbcc04
Daniel M. Tarrio Guri <daniel.tarrio@gmail.com>
2024-01-17 22:41:26 -0300

    cambios de estilo, optimizacion reproductor

--- js.js
+++ js.js
@@ -2,24 +2,26 @@
 /*{}*/
 let video="";
 let mi_intervalo="";
+let ctrl="";
 
 const reproducir=(x,y)=>{
     video=document.getElementById(y);
     let dur_min=parseInt(video.duration/60);
     let dur_seg=parseInt(video.duration-(dur_min*60));
+    ctrl=x;
     const number = 1
     dur_min=dur_min.toString().padStart(2, '0');
     dur_seg=dur_seg.toString().padStart(2, '0');
     document.getElementById("duracion").innerText=dur_min+":"+dur_seg;
     if(video.paused==false){
         video.pause();
-        x.classList.add('ctrl_play');
-        x.classList.remove('ctrl_pause'); //no utilizo toogle, producia errores al cambiar mas de una vez
+        ctrl.classList.add('ctrl_play');
+        ctrl.classList.remove('ctrl_pause'); //no utilizo toogle, producia errores al cambiar mas de una vez
         stop_temporizador();
     }else{
         video.play();
-        x.classList.remove('ctrl_play');
-        x.classList.add('ctrl_pause');
+        ctrl.classList.remove('ctrl_play');
+        ctrl.classList.add('ctrl_pause');
         temporizador()
     }
 }
@@ -32,8 +34,11 @@ const temporizador=()=> {
     rep_seg=rep_seg.toString().padStart(2, '0');
     document.getElementById("reproducido").innerText=rep_min+":"+rep_seg;
     document.getElementById("mi_barra").value=parseFloat((video.currentTime/video.duration)*100);
-    if(video.duration==video.currentTime){
+    if(video.duration==video.currentTime){ /* se pude reemplazar por video.ended*/
         stop_temporizador();
+        ctrl.classList.add('ctrl_play');
+        ctrl.classList.remove('ctrl_pause');
+        video.load()
     }
 }
 
@@ -44,8 +49,10 @@ const avance=(x)=>{
 
 const maximizar=(y)=>{
     video=document.getElementById(y);
-   /* video.width = 1000; */
-    video.fullscreen();
+   /* video.width = 1000; */video
+   if (video.requestFullscreen) {
+    video.requestFullscreen();
+    }
     console.dir(video);
 }
 
